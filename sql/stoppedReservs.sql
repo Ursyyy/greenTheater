439851bef6510626e750bec5547396361294d1b7
Ursyyy <al3xurslul@gmail.com>
2021-05-25 16:56:09 +0300

    Change stoppedReservs

--- sql/stoppedReservs.sql
+++ sql/stoppedReservs.sql
@@ -2,8 +2,8 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET SQL_NOTES=0 */;
-DROP TABLE IF EXISTS stoppedreservs;
-CREATE TABLE `stoppedreservs` (
+DROP TABLE IF EXISTS stoppedReservs;
+CREATE TABLE `stoppedReservs` (
   `id` int NOT NULL AUTO_INCREMENT,
   `startTime` datetime DEFAULT NULL,
   `endTime` datetime DEFAULT NULL,
